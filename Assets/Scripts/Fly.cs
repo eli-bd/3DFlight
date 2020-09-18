154262db1f38e2273518a19c3c15aedeb4e1075a
eli-bd <elibendavid88@gmail.com>
2020-09-18 05:45:38 +0300

    First commit

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    public float movementSpeed = 800;
+    public float rotationSpeed = 100.0f;
+    private Rigidbody rbody;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Update the rigidbody's rotation
+        float rotationFactor = rotationSpeed * Time.fixedDeltaTime;
+        float pitch = Input.GetAxis("Vertical") * rotationFactor;
+        float yaw = Input.GetAxis("Horizontal") * rotationFactor;
+        float roll = Input.GetAxis("Roll") * rotationFactor;
+        Quaternion rotation = Quaternion.identity;
+        rotation.eulerAngles = new Vector3(-pitch, yaw, roll); // To invert keys add a minus before pitch/yaw/roll
+        rbody.rotation *= rotation;
+
+        // Update the rigidbody's velocity
+        Vector3 position = rbody.rotation * Vector3.forward;
+        rbody.velocity = position * movementSpeed * Time.fixedDeltaTime;
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# How to use this in your own project
+* Create an object
+* Add component >> RigidBody
+* In the RigidBody >> Constraints >> Freeze rotation of x, y and z planes
+* Attach the fly.cs script to the object
+* Edit >> Project Settings >> Input Manager >> Set Horizontal [q][e] (for yaw), Set Vertical [w][s] (for pitch), Define "Roll" [a][d]
